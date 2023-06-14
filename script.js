7d9b0bb1dc48a25ae17c5fe5f626cfdf55c0bee2
Sheptacode <sheptacode@gmail.com>
2023-06-14 19:12:42 +0700

    menambahkan file script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let burger = document.querySelector('.burger');
+let navlinks = document.getElementById('.nav-links');
+let links = document.querySelectorAll('.links');
+
+burger.addEventListener('click', () => {
+	navlinks.classList.toggle('.hide');
+	burger.classList.toggle('lines-rotate');
+});
+
+
+for (let i = 0; 1 < )
+
+
+
+console.log("ok");

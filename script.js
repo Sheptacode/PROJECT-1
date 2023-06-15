d41e6015e75bb64b5f367f6ad2751c633a79e679
Sheptacode <sheptacode@gmail.com>
2023-06-15 02:54:20 -0700

    merubah navbar

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ let navlinks = document.getElementById('.nav-links');
 let links = document.querySelectorAll('.links');
 
 burger.addEventListener('click', () => {
-	navlinks.classList.toggle('.hide');
+	navlinks.classList.toggle('hide');
 	burger.classList.toggle('lines-rotate');
 });
 
 
-for (let i = 0; 1 < )
+for (let i = 0; 1 < links.length; i++) {
+	links[i].addEventListener('click', () => {
+		navlinks.classList.toggle('hide');
+	});
+}
 
 
 

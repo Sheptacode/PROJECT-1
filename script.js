aa202a88df0a1dbfff0e09ab451717c3f67cd5d6
Sheptacode <sheptacode@gmail.com>
2023-06-16 02:20:13 -0700

    menyelesaikan navbar

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 let burger = document.querySelector('.burger');
-let navlinks = document.getElementById('.nav-links');
+let navlinks = document.getElementById('nav-links');
 let links = document.querySelectorAll('.links');
 
+burger.addEventListener('click', () => {
+	navlinks.classList.toggle('hide');
+	burger.classList.toggle('lines-rotate');
+})
 
-
-
-for (let i = 0; 1 < links.length; i++) {
+for (let i = 0; i < links.length; i++) {
 	links[i].addEventListener('click', () => {
 		navlinks.classList.toggle('hide');
-	});
+	})
 }
-
-
-console.log("ok");
